8a1629218df202e1d157de2425f3e1c4ca8d4606
Ivan Gubarev <lippevan@gmail.com>
2023-04-03 16:51:02 +0300

    add prime game

---
+++ src/games/prime-game.js
@@ -0,0 +1,27 @@
+import gameEngine from '../index.js';
+import getRandomInt from '../get-random-int.js';
+
+const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const gameCore = () => {
+  const num = getRandomInt(100);
+  const validAnswer = isPrime(num) ? 'yes' : 'no';
+  const question = `${num}`;
+  return [question, validAnswer];
+};
+
+const primeGame = () => gameEngine(gameTask, gameCore);
+
+export default primeGame;
